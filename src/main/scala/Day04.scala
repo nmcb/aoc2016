eaa621d87692201e8da937d8d90ce7dad9719e16
nmcb <nmcb@xs4all.nl>
2025-05-12 13:54:18 +0200

    cleanup day 04

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -1,9 +1,9 @@
+import scala.annotation.tailrec
 import scala.io.*
 
 object Day04 extends App:
 
-  val day: String =
-    this.getClass.getName.drop(3).init
+  val day = getClass.getSimpleName.filter(_.isDigit).mkString
 
   case class Room(underlying: String):
 
@@ -20,12 +20,17 @@ object Day04 extends App:
 
     def isValid: Boolean =
 
-      implicit val ordering: Ordering[(Char,Int)] =
+      type Chars = (Char,Int)
+
+      extension (chars: Chars)
+        def char: Char = chars._1
+        def index: Int = chars._2
+
+      given ordering: Ordering[Chars] =
         Ordering
-          .by[(Char,Int),Int](_._2)
+          .by[Chars,Int](_.index)
           .reverse
-          .orElse(Ordering
-            .by[(Char,Int),Int](_._1))
+          .orElse(Ordering.by[Chars,Int](_.char))
 
       val common: List[Char] =
         name
@@ -33,12 +38,14 @@ object Day04 extends App:
           .groupMapReduce(identity)(_ => 1)(_ + _)
           .toList
           .sorted
-          .map(_._1)
+          .map(_.char)
           .take(5)
 
       checksum.forall(common.contains)
 
     def decrypt: String =
+
+      @tailrec
       def rotate(c: Char, i: Int): Char =
         if c > 'z' then rotate('a', i) else if i == 0 then c else rotate((c + 1).toChar, i - 1)
 
@@ -48,7 +55,9 @@ object Day04 extends App:
 
 
   object Room:
-    def fromLine(s: String): Room = Room(s)
+
+    def fromLine(s: String): Room =
+      Room(s)
 
   val rooms: List[Room] =
     Source
@@ -57,10 +66,10 @@ object Day04 extends App:
       .map(Room.fromLine)
       .toList
 
-  val start1: Long = System.currentTimeMillis
-  val answer1: Int = rooms.filter(_.isValid).map(_.id).sum
-  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+  val start1  = System.currentTimeMillis
+  val answer1 = rooms.filter(_.isValid).map(_.id).sum
+  println(s"Answer day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
-  val start2: Long = System.currentTimeMillis
-  val answer2: Int = rooms.find(_.decrypt.trim == "northpole object storage").get.id
-  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
+  val start2  = System.currentTimeMillis
+  val answer2 = rooms.find(_.decrypt.trim == "northpole object storage").get.id
+  println(s"Answer day $day part 2: $answer2 [${System.currentTimeMillis - start2}ms]")
