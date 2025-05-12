a25ad08156267584e71a38c0f750f5d28cc2149e
nmcb <nmcb@xs4all.nl>
2025-05-12 14:12:13 +0200

    cleanup day 08

--- src/main/scala/Day08.scala
+++ src/main/scala/Day08.scala
@@ -1,36 +1,36 @@
 import scala.io.*
-import scala.util.*
 
 object Day08 extends App:
 
-  val day: String = this.getClass.getName.drop(3).init
+  val day = getClass.getSimpleName.filter(_.isDigit).mkString
 
-  enum Op:
+  enum Operation:
     case Rectangle(sizeX: Int, sizeY: Int)
     case RotateCol(index: Int, by: Int)
     case RotateRow(index: Int, by: Int)
 
-  object Op:
-    def fromString(s: String): Op =
+  object Operation:
+    def fromString(s: String): Operation =
       s match
         case s"rect ${sizeX}x${sizeY}"        => Rectangle(sizeX = sizeX.toInt, sizeY = sizeY.toInt)
         case s"rotate column x=$index by $by" => RotateCol(index = index.toInt, by = by.toInt)
         case s"rotate row y=$index by $by"    => RotateRow(index = index.toInt, by = by.toInt)
 
-  val operations: List[Op] =
+  val operations: Vector[Operation] =
     Source
       .fromResource(s"input$day.txt")
       .getLines
-      .map(Op.fromString)
-      .toList
+      .map(Operation.fromString)
+      .toVector
 
   object Screen:
-    def empty: Screen =
+
+    val empty: Screen =
       Screen(List.fill(6, 50)('.'))
 
 
   case class Screen(pixels: List[List[Char]]):
-    import Op.*
+    import Operation.*
 
     val sizeX: Int = pixels.head.size
     val sizeY: Int = pixels.size
@@ -38,7 +38,7 @@ object Day08 extends App:
     def countLit: Int =
       pixels.map(_.count(_ == '#')).sum
 
-    override def toString: String =
+    def asString: String =
       pixels.map(_.mkString).mkString("\n")
 
     def transpose: Screen =
@@ -55,7 +55,7 @@ object Day08 extends App:
       val shifted       = Iterator.continually(row.head).flatten.drop(sizeX - by).take(sizeX).toList
       Screen(top :+ shifted :++ rest)
 
-    infix def process(op: Op): Screen =
+    infix def process(op: Operation): Screen =
       op match
         case Rectangle(sizeX, sizeY) => rectangle(sizeX, sizeY)
         case RotateRow(index, by)    => shiftRow(index, by)
@@ -66,5 +66,5 @@ object Day08 extends App:
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
   val start2: Long    = System.currentTimeMillis
-  val answer2: String = operations.foldLeft(Screen.empty)(_ process _).toString
+  val answer2: String = operations.foldLeft(Screen.empty)(_ process _).asString
   println(s"Answer day $day part 2:\n${answer2} [${System.currentTimeMillis - start2}ms]")
