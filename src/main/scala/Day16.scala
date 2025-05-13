09109672c6a935033e3311c4c1fb1f93c23eee5d
nmcb <nmcb@xs4all.nl>
2025-05-13 13:42:29 +0200

    performance enhancements

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -4,14 +4,12 @@ object Day16 extends App:
 
   @tailrec
   def dragon(s: String, length: Int): String =
-    if s.length >= length then
-      s.take(length)
-    else
-      dragon(s + '0' + s.reverse.map(c => if c == '0' then '1' else '0'), length)
+    val result = s + '0' + s.reverse.map(c => if c == '0' then '1' else '0')
+    if result.length >= length then result.take(length) else dragon(result, length)
 
   @tailrec
   def checksum(s: String): String =
-    val result = s.grouped(2).map(t => if t(0) == t(1) then '1' else '0').mkString
+    val result = s.grouped(2).map(p => if p(0) == p(1) then '1' else '0').mkString
     if result.length % 2 == 0 then checksum(result) else result
 
   val day = getClass.getSimpleName.filter(_.isDigit).mkString
